b76cd773a85e516c0d781f74d74e08fe1b3de897
zhanglu <550535488@qq.com>
2018-11-12 19:21:00 +0800

    计数器

--- vue-demo/src/App.vue
+++ vue-demo/src/App.vue
@@ -1,28 +1,44 @@
 <template>
-  <div id="app">
-    <img src="assets/logo.png">
-    <HelloWorld/>
+  <div>
+                                     <!--计算属性（根据count来决定-->
+    <p>click {{count}} times count is {{evenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    data () {
+      return {
+        //用vuex来管理这个count状态
+        count: 0
+      }
+    },
+    //写四个事件回调函数，view触发的事件
+    methods: {
+      increment () {
+        //这个方法的功能
+        this.count++
+      },
+      decrement () {
+        this.count--
+      },
+      incrementIfOdd () {
+        if (this.count % 2 === 1) {
+          this.count++
+        }
+      },
+      incrementAsync () {
+        setTimeout(() => {
+          this.count++
+        },500)
+      },
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
